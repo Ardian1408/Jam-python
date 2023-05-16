cc0bcefe427e8b8cb412778270b69ece4a83e2f6
Ardian1408 <ardi3665@gmail.com>
2023-05-16 22:28:28 +0700

    init background jam update

--- jam.py
+++ jam.py
@@ -9,6 +9,26 @@ def update_clock():
     clock_label.config(text=current_time)
     clock_label.after(1000, update_clock)
 
+def drag_start(event):
+    # Menyimpan posisi awal saat penyeretan dimulai
+    canvas.data = {'x': event.x, 'y': event.y}
+
+def drag_move(event):
+    # Menghitung perubahan posisi saat penyeretan berlangsung
+    dx = event.x - canvas.data['x']
+    dy = event.y - canvas.data['y']
+
+    # Memindahkan tampilan jam sesuai dengan perubahan posisi
+    canvas.move(clock_text, dx, dy)
+
+    # Memperbarui posisi awal untuk iterasi berikutnya
+    canvas.data['x'] = event.x
+    canvas.data['y'] = event.y
+
+def resize(event):
+    # Mengubah ukuran tampilan jam sesuai dengan ukuran persegi panjang
+    canvas.itemconfigure(clock_text, font=('Arial', event.width // 10))
+
 def change_background():
     file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg")])
     if file_path:
@@ -38,9 +58,25 @@ def change_background():
         except:
             tk.messagebox.showerror("Error", "Failed to open the selected image.")
 
+def change_clock_position():
+    new_x = int(input("Masukkan koordinat X baru: "))
+    new_y = int(input("Masukkan koordinat Y baru: "))
+    clock_label.place(x=new_x, y=new_y)  # Mengubah posisi tampilan jam
+
 # Membuat jendela utama
 window = tk.Tk()
 window.title("Jam Digital")
+
+# Membuat canvas
+canvas = tk.Canvas(window, width=600, height=400)
+canvas.pack()
+
+# Membuat persegi panjang sebagai kontrol untuk mengubah ukuran jam
+resize_rect = canvas.create_rectangle(500, 300, 600, 400, fill='gray', tags='resize')
+canvas.tag_bind('resize', '<B1-Motion>', resize)
+
+# Membuat label untuk menampilkan jam
+clock_text = canvas.create_text(300, 200, text='', font=('Arial', 60), anchor='center')
 
 # Mengatur latar belakang
 default_image = Image.open("default_background.png")
@@ -56,6 +92,10 @@ change_background_button.pack(pady=10)
 # Membuat label untuk menampilkan jam
 clock_label = tk.Label(window, font=("Arial", 80))
 clock_label.pack(padx=50, pady=50)
+
+# Tombol untuk mengubah posisi tampilan jam
+change_position_button = tk.Button(window, text="Change Clock Position", command=change_clock_position)
+change_position_button.pack()
 
 # Memanggil fungsi update_clock() untuk pertama kali
 update_clock()
